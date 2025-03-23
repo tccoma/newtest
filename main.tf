d511333521fb561998b750ca8ab50215165d1947
tccoma <67564396+tccoma@users.noreply.github.com>
2025-03-23 15:50:07 +1100

    initial commit

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    fortios = {
+      source  = "fortinetdev/fortios"
+    }
+  }
+}
+
+provider "fortios" {
+  hostname     = "192.168.52.177"
+  token        = "jn3t3Nw7qckQzt955Htkfj5hwQ6jdb"
+  insecure     = "true"
+}
+
+resource "fortios_system_dns" "trname" {
+  cache_notfound_responses = "disable"
+  dns_cache_limit          = 5000
+  dns_cache_ttl            = 1800
+  ip6_primary              = "::"
+  ip6_secondary            = "::"
+  primary                  = "208.91.112.53"
+  retry                    = 2
+  secondary                = "208.91.112.51"
+  source_ip                = "0.0.0.0"
+  timeout                  = 5
+}
